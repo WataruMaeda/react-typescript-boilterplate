b63a064838d57cf243e5b2395363ed1a00239ef1
WataruMaeda <w.maeda.jp@gmail.com>
2021-03-17 15:45:18 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const name = 'aaa'
+  let name = 'aaa'
   return (
     <div className="App">
       <header className="App-header">
