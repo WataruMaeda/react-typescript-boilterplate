b9521c8a20f434328d7fd4ea33e2735b3de363bd
WataruMaeda <w.maeda.jp@gmail.com>
2021-04-02 20:05:16 -0700

    chore: configure css in js

--- src/scenes/dashboard/Dashboard.tsx
+++ src/scenes/dashboard/Dashboard.tsx
@@ -1,12 +1,12 @@
 import Button from 'components/Button'
 import { actions, selector } from 'modules/app.module'
 import { useDispatch, useSelector } from 'utils/connector'
-import logo from 'assets/images/logo.svg'
+import { images } from 'theme'
 import styles from './dashboard.module.scss'
 
 const Dashboard = () => {
-  const { loggedIn } = useSelector(selector)
   const dispatch = useDispatch()
+  const { loggedIn } = useSelector(selector)
 
   const handleUpdateLogin = () => {
     dispatch(actions.setLoginAsync())
@@ -15,7 +15,7 @@ const Dashboard = () => {
   return (
     <div className={styles.root}>
       <header>
-        <img src={logo} className={styles.logo} alt="logo" />
+        <img src={images.logo} className={styles.logo} alt="logo" />
         <p>{`Dashboard: login status: ${loggedIn}`}</p>
         <Button label="Update Login Status" onClick={handleUpdateLogin} />
       </header>
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+import styler from './styler'
+import colors from './colors'
+import images from './images'
+import breakpoints from './breakpoints'
+import rem from './rem'
+
+export { styler, colors, images, breakpoints, rem }
